7f84f1b4def87a150d0defff9a29efdfd24b9469
Nathaniel Mosco <natemosco@gmail.com>
2019-10-04 12:38:11 -0400

    created card function

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,27 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+$ = s => document.createElement(s)
+$$ = s => document.querySelector(s)
+$$$ = s => document.querySelectorAll(s)
+function Cards(obj) {
+    let card = $("div")
+    card.classList.add("card")
+    let div = $("div")
+    div.classList.add("headline")
+    div.textContent = "headline"
+    let div2 = $("div")
+    div2.classList.add("author")
+    let div3 = $("div")
+    div3.classList.add("img-container")
+    let img = $("img")
+    img.src = "url"
+    let span = $("span")
+    span.textContent = `By authorname`
+
+    card.append(div, div2)
+    div2.append(div3, span)
+    div3.append(img)
+
+    return card
+}
